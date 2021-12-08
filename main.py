7b0ed5be404f66fd7eacb255ca73b08658a8c5f4
xTecna <raquelmarcolino25@gmail.com>
2021-12-08 16:16:44 -0300

    New expansion and now a mode when you don't want to download images.

--- main.py
+++ main.py
@@ -7,13 +7,12 @@
 from zipfile import ZipFile
 import urllib.request
 import shutil
-import codecs
 import json
-import sys
 import io
 import os
 
 card_data = []
+
 
 class CardSet:
     def __init__(self, images_folder, card_set, language):
@@ -22,22 +21,30 @@ class CardSet:
         self.folder = card_set['folder']
         self.language = language
 
-        self.images_path = os.path.join(os.path.dirname(__file__), self.folder, self.language, 'img', 'cards')
-        self.file_path = os.path.join(os.path.dirname(__file__), self.folder, self.language, 'data', '{0}-{1}.json'.format(self.folder, self.language))
+        self.images_path = os.path.join(os.path.dirname(
+            __file__), self.folder, self.language, 'img', 'cards')
+        self.file_path = os.path.join(os.path.dirname(
+            __file__), self.folder, self.language, 'data', '{0}-{1}.json'.format(self.folder, self.language))
         self.output_folder = os.path.join(images_folder, self.language)
+
 
 class Config:
     def __init__(self, config, config_language, config_champion_names):
         self.output_folder = config['output_folder']
-        self.images_folder = os.path.join(self.output_folder, config['images_folder'])
-        self.output_file = os.path.join(self.output_folder, config['output_file'])
+        self.images_folder = os.path.join(
+            self.output_folder, config['images_folder'])
+        self.output_file = os.path.join(
+            self.output_folder, config['output_file'])
+        self.include_images = config['include_images'] == 'yes'
 
         self.card_sets = config['card_sets']
         self.dictionary = config_language
         self.champion_names = config_champion_names
 
+
 def getPath(file):
-	return os.path.join(os.path.dirname(__file__), file)
+    return os.path.join(os.path.dirname(__file__), file)
+
 
 def setConfig():
     config = None
@@ -48,45 +55,57 @@ def setConfig():
             config_language = json.load(file_language)
             with io.open(getPath('champion_names.json'), 'r', encoding='utf-8') as file_champion_names:
                 config_champion_names = json.load(file_champion_names)
-                config = Config(config_file, config_language, config_champion_names)
+                config = Config(config_file, config_language,
+                                config_champion_names)
 
     return config
 
-def getDataFromCardSet(card_set, dictionary, champion_names):
+
+def getDataFromCardSet(card_set, include_images, dictionary, champion_names):
     file_zip = getPath('{0}.zip'.format(card_set.folder))
-    
+
     urllib.request.urlretrieve(card_set.url, file_zip)
-    print('Files from set {0} downloaded successfully, resulting in file {1}.'.format(card_set.name, file_zip))
-    
+    print('Files from set {0} downloaded successfully, resulting in file {1}.'.format(
+        card_set.name, file_zip))
+
     with ZipFile(file_zip, 'r') as zipObj:
-        zipObj.extractall(getPath(card_set.folder))
+        if(include_images):
+            zipObj.extractall(getPath(card_set.folder))
+        else:
+            zipObj.extract('{0}/data/{1}-{0}.json'.format(card_set.language,
+                           card_set.folder), getPath(card_set.folder))
     print('Extraction of {0} finished.'.format(file_zip))
-    
+
     os.remove(file_zip)
-    
+
     with io.open(card_set.file_path, 'r', encoding='utf8') as file_json:
         fileData = json.load(file_json)
         for data in fileData:
             card_data.append(registerCard(dictionary, champion_names, data))
     print('Data from cards from set {0} obtained.'.format(card_set.name))
-    
-    if (not os.path.isdir(card_set.output_folder)):
-        os.mkdir(card_set.output_folder)
-    
-    images = os.listdir(card_set.images_path)
-    for image in images:
-        source = os.path.join(card_set.images_path, image)
-        destination = os.path.join(card_set.output_folder, image)
-        shutil.move(source, destination)
-    print('All card images from set {0} were transferred to folder {1}.'.format(card_set.name, card_set.output_folder))
+
+    if (include_images):
+        if (not os.path.isdir(card_set.output_folder)):
+            os.mkdir(card_set.output_folder)
+
+        images = os.listdir(card_set.images_path)
+        for image in images:
+            source = os.path.join(card_set.images_path, image)
+            destination = os.path.join(card_set.output_folder, image)
+            shutil.move(source, destination)
+        print('All card images from set {0} were transferred to folder {1}.'.format(
+            card_set.name, card_set.output_folder))
 
     shutil.rmtree(card_set.folder, ignore_errors=True)
 
+
 def registerCard(dictionary, champion_names, card):
     regionRefs = [x.lower() for x in card['regionRefs']]
-    nameRef = champion_names[card['cardCode']] if card['cardCode'] in champion_names else ''
+    nameRef = champion_names[card['cardCode']
+                             ] if card['cardCode'] in champion_names else ''
     subtypes = [dictionary['subtypes'][x] for x in card['subtypes']]
-    supertype = dictionary['types'][card['supertype']] if card['supertype'] != '' else ''
+    supertype = dictionary['types'][card['supertype']
+                                    ] if card['supertype'] != '' else ''
     cardType = dictionary['types'][card['type']]
 
     return {
@@ -114,19 +133,24 @@ def registerCard(dictionary, champion_names, card):
         'set': card['set'].lower()
     }
 
+
 config = setConfig()
-if (not os.path.isdir(config.images_folder)):
+if (config.include_images and not os.path.isdir(config.images_folder)):
     os.mkdir(config.images_folder)
 for language in config.dictionary:
     card_data = []
     card_set_object = None
     for card_set in config.card_sets:
         card_set_object = CardSet(config.images_folder, card_set, language)
-        print('Downloading files from set {0} in language {1}. This step may take some minutes.'.format(card_set_object.name, language))
-        getDataFromCardSet(card_set_object, config.dictionary[language], config.champion_names)
+        print('Downloading files from set {0} in language {1}. This step may take some minutes.'.format(
+            card_set_object.name, language))
+        getDataFromCardSet(
+            card_set_object, config.include_images, config.dictionary[language], config.champion_names)
     output_file = config.output_file.format(language)
     with io.open(output_file, 'w', encoding='utf8') as output:
         output.write(json.dumps(card_data, ensure_ascii=False))
-    print('All data collected from set {0} in language {1}.'.format(card_set_object.name, language))
+    print('All data collected from set {0} in language {1}.'.format(
+        card_set_object.name, language))
 
-print('The script finished sucessfully. All data can be found at file {0}. All card images are at folder {1}.'.format(config.output_file, config.images_folder))
+print('The script finished sucessfully. All data can be found at file {0}.{1}'.format(
+    config.output_file, ' All card images are at folder {0}.'.format(config.images_folder) if config.include_images else ''))
